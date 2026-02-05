4740e3cbddc8385fde7112e36d605081cb1861cd
Gohil Gautam <gohil.g.m9764@gmail.com>
2026-02-05 08:13:19 +0530

    teg selet html in js

---
+++ app.js
@@ -0,0 +1,5 @@
+let smg = window.document.getElementById("msg");
+
+let home = window.document.getElementById("Home");
+let Profile = window.document.getElementById("Profile");
+let Settings = window.document.getElementById("Settings");
